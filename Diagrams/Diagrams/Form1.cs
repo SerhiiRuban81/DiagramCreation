e766fb505ce63de3843997a51b10c085691ccbd4
RubanSA <ruban@kr.step>
2019-12-06 19:20:49 +0200

    Second btn added

--- Diagrams/Diagrams/Form1.cs
+++ Diagrams/Diagrams/Form1.cs
@@ -21,5 +21,10 @@ namespace Diagrams
         {
             MessageBox.Show("Hello git!");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Second btn!");
+        }
     }
 }
